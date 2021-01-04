7695eab2dab90bb4c9c17150472f1d799cc76af4
Stefan <sjf.de.jong@gmail.com>
2021-01-04 23:01:52 +0100

    setting up comakership store

--- src/components/Comakerships.vue
+++ src/components/Comakerships.vue
@@ -1,6 +1,14 @@
 <template>
 <div>
-
+<v-container class="my-4">
+    <h2>List of Comakerships</h2>
+    <v-data-table
+      :headers="headers"
+      :items="comakerships"
+      :items-per-page="15"
+      class="elevation-1"
+    ></v-data-table>    
+  </v-container>
 </div>
 </template>
 
