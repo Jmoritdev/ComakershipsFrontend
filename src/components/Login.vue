e43b9a752f5e90063a64db01eb2e0ed110448b26
Kayleigh Vossen <585910@student.inholland.nl>
2021-01-05 15:50:24 +0100

    store data gets loaded

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,28 +1,32 @@
 <template>
   <div>
-    <v-card v-if="!$store.state.isAuthenticated" ref="form">
-      <v-card-text>
-        <v-alert v-if="authData.error" type="error">
-          {{ authData.error }}
-        </v-alert>
-        <v-alert v-if="authData.success" type="success">
-          {{ authData.success }}
-        </v-alert>
-        <v-text-field
-            v-model="authData.email"
-            label="Email"
-        ></v-text-field>
-        <v-text-field
-            v-model="authData.password"
-            type="Password"
-            label="Password"
-        ></v-text-field>
-        <v-btn color="primary" @click="login"> Login</v-btn>
-      </v-card-text>
-    </v-card>
-    <v-card v-if="$store.state.isAuthenticated">
-      <h1> You are already logged in, explore the app through the menu on the left. </h1>
-    </v-card>
+    <div v-if="$store.state.isAuthenticated === false">
+      <v-card ref="form">
+        <v-card-text>
+          <v-alert v-if="authData.error" type="error">
+            {{ authData.error }}
+          </v-alert>
+          <v-alert v-if="authData.success" type="success">
+            {{ authData.success }}
+          </v-alert>
+          <v-text-field
+              v-model="authData.email"
+              label="Email"
+          ></v-text-field>
+          <v-text-field
+              v-model="authData.password"
+              type="Password"
+              label="Password"
+          ></v-text-field>
+          <v-btn color="primary" @click="login"> Login</v-btn>
+        </v-card-text>
+      </v-card>
+    </div>
+    <div v-if="$store.state.isAuthenticated === true">
+      <v-card>
+        <h1> You are already logged in, explore the app through the menu on the left. </h1>
+      </v-card>
+    </div>
   </div>
 </template>
 
